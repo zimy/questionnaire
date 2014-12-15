106ac292e68ebe0a6116d6041c09baf0df4a06a2
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2014-12-16 01:19:25 +0300

    Added mailSending beans and configuration in yaml with format: mail:   host:   port:   email:   ssl:   username:   password:   recipients:     -   theme:   text:

---
+++ src/main/java/me/zimy/questionnaire/MailSenderConfiguration.java
@@ -0,0 +1,94 @@
+package me.zimy.questionnaire;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Dmitriy &lt;Zimy&gt; Yakovlev
+ * @since 12/15/14.
+ */
+@Configuration
+@ConfigurationProperties(prefix = "mail")
+public class MailSenderConfiguration {
+    String host = "";
+    Integer port = 0x0FACADED;
+    String username = "";
+    String password = "";
+    String email = "";
+    String theme = "Questionnaire completed";
+    String text = "";
+    boolean ssl = false;
+    List<String> recipients = new ArrayList<>();
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.auth", "true");
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.starttls.enable", "true");
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.**ssl.enable", String.valueOf(ssl));
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.**ssl.required", String.valueOf(ssl));
+        javaMailSender.getJavaMailProperties().setProperty("mail.debug", "true");
+        javaMailSender.getJavaMailProperties().setProperty("mail.transport.protocol", ssl ? "smtps" : "smtp");
+        return javaMailSender;
+    }
+
+    @Bean
+    public SimpleMailMessage templateMessage() {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(email);
+        simpleMailMessage.setSubject(theme);
+        simpleMailMessage.setText(text);
+        return simpleMailMessage;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public void setSsl(boolean ssl) {
+        this.ssl = ssl;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<String> getRecipients() {
+        return recipients;
+    }
+}
