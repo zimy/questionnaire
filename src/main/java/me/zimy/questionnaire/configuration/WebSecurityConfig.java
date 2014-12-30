6085b90c722ebcd9a7f1a234f66c640bf6b0694f
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2014-12-31 01:31:07 +0300

    Securing only services with standard password and login pair

---
+++ src/main/java/me/zimy/questionnaire/configuration/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package me.zimy.questionnaire.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/webjars/**", "/common.css").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
