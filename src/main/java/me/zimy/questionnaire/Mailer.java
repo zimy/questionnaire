83c8d40e9dd96f724825a64b15df5b4ad1137d21
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2015-01-09 01:08:32 +0300

    Report mailing moved to separate class

--- src/main/java/me/zimy/questionnaire/Mailer.java
+++ src/main/java/me/zimy/questionnaire/Mailer.java
@@ -1,22 +1,16 @@
 package me.zimy.questionnaire;
 
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
-import me.zimy.questionnaire.configuration.*;
 import me.zimy.questionnaire.domain.Responder;
 import me.zimy.questionnaire.mails.NotificationPreparer;
+import me.zimy.questionnaire.mails.ReportPreparer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import javax.mail.internet.MimeMessage;
 import java.nio.file.Path;
-import java.text.DateFormat;
-import java.util.Date;
 
 /**
  * Service to send emails on some actions
@@ -29,25 +23,11 @@ import java.util.Date;
 public class Mailer {
     private final Logger logger = LoggerFactory.getLogger(Mailer.class);
     @Autowired
-    DomainConfiguration domainConfiguration;
-    @Autowired
-    GenderConfiguration genderConfiguration;
-    @Autowired
-    LikenessConfiguration likenessConfiguration;
-    @Autowired
-    RecipientList recipientList;
-    @Autowired
-    NotificationConfiguration notificationConfiguration;
-    @Autowired
     JavaMailSender mailSender;
     @Autowired
-    MailDaemonConfiguration senderConfiguration;
-    @Autowired
-    RequestReportConfiguration requestReportConfiguration;
-    @Autowired
-    ScheduledReportConfiguration scheduledReportConfiguration;
-    @Autowired
     NotificationPreparer notificationPreparer;
+    @Autowired
+    ReportPreparer reportPreparer;
 
     void notifyOnResponderDone(final Responder responder) {
         try {
@@ -59,25 +39,7 @@ public class Mailer {
 
     public void emailReport(final Path path, final boolean requested) {
         try {
-            mailSender.send(new MimeMessagePreparator() {
-                @Override
-                public void prepare(MimeMessage mimeMessage) throws Exception {
-                    Date date = new Date();
-                    DateFormat df = new ISO8601DateFormat();
-                    String format = df.format(date);
-                    MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-                    mimeMessageHelper.setTo(recipientList.getRecipients().toArray(new String[recipientList.getRecipients().size()]));
-                    mimeMessageHelper.addAttachment("Report at " + format + ".ods", path.toFile());
-                    mimeMessageHelper.setFrom(senderConfiguration.getEmail());
-                    if (requested) {
-                        mimeMessageHelper.setSubject(requestReportConfiguration.getSubject());
-                        mimeMessageHelper.setText(String.format(requestReportConfiguration.getText(), format));
-                    } else {
-                        mimeMessageHelper.setSubject(scheduledReportConfiguration.getSubject());
-                        mimeMessageHelper.setText(String.format(scheduledReportConfiguration.getText(), format));
-                    }
-                }
-            });
+            mailSender.send(reportPreparer.requestReport(path, requested));
         } catch (Exception e) {
             logger.error("Can't email: " + e.getMessage());
 
---
+++ src/main/java/me/zimy/questionnaire/mails/ReportPreparer.java
@@ -0,0 +1,54 @@
+package me.zimy.questionnaire.mails;
+
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import me.zimy.questionnaire.configuration.MailDaemonConfiguration;
+import me.zimy.questionnaire.configuration.NotificationConfiguration;
+import me.zimy.questionnaire.configuration.RecipientList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * @author Dmitriy &lt;Zimy&gt; Yakovlev
+ * @since 1/9/15.
+ */
+@Component
+public class ReportPreparer {
+    @Autowired
+    private RecipientList recipientList;
+    @Autowired
+    private MailDaemonConfiguration senderConfiguration;
+    @Autowired
+    private NotificationConfiguration requestReportConfiguration;
+    @Autowired
+    private NotificationConfiguration scheduledReportConfiguration;
+
+    public MimeMessagePreparator requestReport(final Path path, final boolean requested) {
+        return new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                Date date = new Date();
+                DateFormat df = new ISO8601DateFormat();
+                String format = df.format(date);
+                String[] to = recipientList.getRecipients().toArray(new String[recipientList.getRecipients().size()]);
+                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                mimeMessageHelper.setTo(to);
+                mimeMessageHelper.addAttachment("Report at " + format + ".ods", path.toFile());
+                mimeMessageHelper.setFrom(senderConfiguration.getEmail());
+                if (requested) {
+                    mimeMessageHelper.setSubject(requestReportConfiguration.getSubject());
+                    mimeMessageHelper.setText(String.format(requestReportConfiguration.getText(), format));
+                } else {
+                    mimeMessageHelper.setSubject(scheduledReportConfiguration.getSubject());
+                    mimeMessageHelper.setText(String.format(scheduledReportConfiguration.getText(), format));
+                }
+            }
+        };
+    }
+}
