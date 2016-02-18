6ba527e4b8b3160aa9ba72b82dc504e16308eb14
Dmitriy Yakovlev <zimy@yandex.ru>
2016-02-18 07:01:53 +0300

    История о том, как я перестал бояться и переписал всё на Kotlin.

--- src/main/kotlin/me/zimy/questionnaire/domain/Responder.kt
+++ src/main/kotlin/me/zimy/questionnaire/domain/Responder.kt
@@ -3,4 +3,4 @@ package me.zimy.questionnaire.domain
 import com.fasterxml.jackson.annotation.JsonManagedReference
 import org.hibernate.validator.constraints.Length
 
-data class Responder(var id: Long, var age: Long, var domain: Domain, var gender: Gender, @Length(min = 1) var identifier: String, @JsonManagedReference var responses: List<Response>)
+data class Responder(var id: Long, var age: Long, var domain: Domain, var gender: Gender, @Length(min = 1) var identifier: String, @JsonManagedReference var responses: MutableList<Response>)
