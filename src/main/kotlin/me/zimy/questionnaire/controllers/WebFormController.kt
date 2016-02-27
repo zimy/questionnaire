10b6df195151b7c92dffc1336bb1a9a07c02e6c8
Dmitriy Yakovlev <zimy@yandex.ru>
2016-02-27 23:45:02 +0300

    Some WTF?

--- src/main/kotlin/me/zimy/questionnaire/controllers/WebFormController.kt
+++ src/main/kotlin/me/zimy/questionnaire/controllers/WebFormController.kt
@@ -1,5 +1,6 @@
 package me.zimy.questionnaire.controllers
 
+import com.google.common.base.Strings
 import me.zimy.questionnaire.DataSaver
 import me.zimy.questionnaire.domain.Responder
 import me.zimy.questionnaire.repositories.QuestionRepository
@@ -8,10 +9,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpSession
 import javax.validation.Valid
 
@@ -38,39 +41,34 @@ class WebFormController {
     private val dataSaver: DataSaver? = null
 
     @RequestMapping(value = "/", method = arrayOf(RequestMethod.GET))
-    fun firstStep(model: Model, responder: Responder): String {
+    fun firstStep(session: HttpSession): String {
+        session.invalidate()
         logger.info("/ GET asked")
         return "commonQuestions"
     }
 
-    @RequestMapping(value = "/", method = arrayOf(RequestMethod.HEAD))
-    fun Head(model: Model, responder: Responder): String {
-        logger.info("/ HEAD asked")
-        return "commonQuestions"
-    }
-
-
-    @RequestMapping(value = "/", method = arrayOf(RequestMethod.POST), params = arrayOf("identifier", "age", "gender", "domain"))
-    fun checkInputFromFirst(@Valid responder: Responder, bindingResult: BindingResult, model: Model, session: HttpSession): String {
-        if (bindingResult.hasErrors()) {
-            logger.error("Binding errors on first POST")
-            return "commonQuestions"
-        }
-        responderService!!.save(responder)
-        logger.info("Responder:" + responder.identifier + " (" + responder.gender + ", " + responder.domain + ", " + responder.age + ") #" + responder.id)
+    @RequestMapping(value = "/", method = arrayOf(RequestMethod.POST), params = arrayOf("identifier", "age", "gender", "domain", "interest"))
+    fun checkInputFromFirst(@Valid responder: Responder, model: Model, session: HttpSession, response: HttpServletResponse): String {
+        val savedResponder = responderService!!.save(responder)
+        logger.info("Responder:" + responder.identifier + " (" + responder.gender + ", " + responder.domain + ", " + responder.age + ") #" + savedResponder.id)
         model.addAttribute("questions", questionService!!.getByTargetGender(responder.gender))
-        session.setAttribute("id", responder.id)
+        session.setAttribute("id", savedResponder.id)
+        response.addCookie(Cookie("id", savedResponder.id))
+        logger.info("Processing with session id = ", session.getAttribute("id").toString())
         return "standardQuestionnaire"
     }
 
     @RequestMapping(value = "/", method = arrayOf(RequestMethod.POST))
     @Throws(InterruptedException::class)
-    fun getSecondStageRequest(@RequestParam params: Map<String, String>, session: HttpSession): String {
-        if (session.getAttribute("id") == null) {
+    fun getSecondStageRequest(@RequestParam params: Map<String, String>, session: HttpSession, request: HttpServletRequest): String {
+        if (Strings.isNullOrEmpty(session.getAttribute("id") as String?)) {
             logger.error("Binding error on second POST")
             return "commonQuestions"
         }
-        val sessionId = session.getAttribute("id").toString()
+
+        val cookieId = request.cookies.filter { one -> "id".equals(one.name) }
+        val sessionId = session.getAttribute("id") as String
+        logger.info("Processing with session id = ", session.getAttribute("id"))
         dataSaver!!.handleSendData(params, sessionId)
         logger.info("#$sessionId completed the survey instance")
         return "thanks"
--- src/main/kotlin/me/zimy/questionnaire/domain/Responder.kt
+++ src/main/kotlin/me/zimy/questionnaire/domain/Responder.kt
@@ -1,5 +1,7 @@
 package me.zimy.questionnaire.domain
 
 import org.hibernate.validator.constraints.Length
+import org.springframework.data.annotation.Id
+import java.util.*
 
-data class Responder(var id: Long, var age: Long, var domain: Domain, var gender: Gender, var interest: Interest, @Length(min = 1) var identifier: String, var responses: MutableList<Response>)
+data class Responder(@Id var id: String? = null, var age: Long = -1, var domain: Domain = Domain.Nothing, var gender: Gender = Gender.Male, var interest: Interest = Interest.Nothing, @Length(min = 1) var identifier: String = "", var responses: MutableList<Response> = ArrayList())
