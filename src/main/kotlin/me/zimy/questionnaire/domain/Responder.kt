ef3b43e8b087d651f22d9780162c383c15ae197c
Dmitriy Yakovlev <zimy@yandex.ru>
2016-03-19 03:13:04 +0300

    Responder now keeps information about third stage

--- src/main/kotlin/me/zimy/questionnaire/domain/Responder.kt
+++ src/main/kotlin/me/zimy/questionnaire/domain/Responder.kt
@@ -3,4 +3,11 @@ package me.zimy.questionnaire.domain
 import org.hibernate.validator.constraints.Length
 import java.util.*
 
-data class Responder(var id: String? = null, var age: Long = -1, var domain: Domain = Domain.Nothing, var gender: Gender = Gender.Male, var interest: Interest = Interest.Nothing, @Length(min = 1) var identifier: String = "", var responses: MutableList<Response> = ArrayList())
+data class Responder(var id: String? = null,
+                     var age: Long = -1,
+                     var domain: Domain = Domain.Nothing,
+                     var gender: Gender = Gender.Male,
+                     var interest: Interest = Interest.Nothing,
+                     @Length(min = 1) var identifier: String = "",
+                     var responses: MutableList<Response> = ArrayList(),
+                     var thirdStages: MutableList<ThirdStageResponse> = ArrayList())
---
+++ src/main/kotlin/me/zimy/questionnaire/domain/ThirdStageResponse.kt
@@ -0,0 +1,6 @@
+package me.zimy.questionnaire.domain
+
+/**
+ * Created by zimy on 3/19/16.
+ */
+data class ThirdStageResponse(val id: String, var response: Int, var thirdPair: ThirdStagePair)
