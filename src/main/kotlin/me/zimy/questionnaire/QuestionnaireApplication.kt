7c9da4459260aceddcd3bc926fde193db99f862d
Dmitriy Yakovlev <zimy@yandex.ru>
2016-04-05 17:58:29 +0300

    Security removed and pseudo-config moved to mudball Kotlin class, gradle wrapper version updated.

--- src/main/kotlin/me/zimy/questionnaire/QuestionnaireApplication.kt
+++ src/main/kotlin/me/zimy/questionnaire/QuestionnaireApplication.kt
@@ -13,3 +13,17 @@ open class QuestionnaireApplication
 fun main(args: Array<String>) {
     SpringApplication.run(QuestionnaireApplication::class.java, *args)
 }
+
+//@Configuration
+//@EnableWebSecurity
+//open class Security(disableDefaults: Boolean) : WebSecurityConfigurerAdapter(disableDefaults) {
+//    override fun configure(http: HttpSecurity) {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/*").permitAll()
+//                .antMatchers("/api/*").hasRole("USER")
+//                .anyRequest().fullyAuthenticated()
+//                .and()
+//                .formLogin();
+//    }
+//}
